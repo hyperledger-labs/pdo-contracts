c9d0415f0170a5e77f62b62d3cf0c4ea1249a1ca
Mic Bowman <mic.bowman@intel.com>
2024-01-31 15:13:31 -0800

    add support for docker-based testing and execution
    Building on the structure used for PDO, this commit adds a recipe for
    building a contracts container that can be used for testing of the
    contracts and the jupyter interface to the contracts.
    
    The structure is similar to the PDO docker support: keys and
    configuration variables are shared through the xfer directory. Tools
    run in the container build the contracts, run the tests, and also run
    a jupyter server.
    
    The behavior can be customized through a local makefile that allows
    for override of variables. A local environment file can be used to
    customize the behavior docker compose.
    
    Signed-off-by: Mic Bowman <mic.bowman@intel.com>

---
+++ docker/pdo_contracts.dockerfile
@@ -0,0 +1,88 @@
+# ------------------------------------------------------------------------------
+# Copyright 2023 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+# PURPOSE: run test suite
+
+# For a complete, automated test environment we need PDO ledger, PDO services, a
+# PDO client that is extended with the contract source. For complete testing we
+# need an OpenVINO model server and the associated guardian service. We can
+# assume that all components are running on localhost. PDO provides the docker
+# and docker compose files that are necessary for running the PDO services.
+# The contracts code must be built from a client image that has the same version
+# as the pdo code in the contracts repository.
+
+# One possibility is to build our own images for PDO ledger, PDO services and
+# PDO client using the pdo submodule in the contracts repo. The advantage of
+# this is that we know the versions will match. THe disadvantage is that it
+# might be a lot of work. Although we might trust docker to "get it right" if we
+# rebuild layers that have already been built. This seems like the best approach.
+
+# When building the client one
+
+# PURPOSE: develop and test contracts with clean
+
+ARG PDO_VERSION
+FROM pdo_client:${PDO_VERSION}
+
+# -----------------------------------------------------------------
+# Install the necessary dependencies
+# -----------------------------------------------------------------
+USER 0:0
+ENV DEBIAN_FRONTEND "noninteractive"
+RUN apt-get update \
+    && apt-get install -y -q \
+    libgl1
+
+# Many of the dependencies should be addressed with the installation
+# of the PDO client including the common contracts, the wasi toolkit
+# and all of the WAMR toolchain
+
+# This should set up all we need for the jupyter server
+ARG UID=1000
+ARG GID=${UID}
+USER ${UID}:${GID}
+RUN --mount=type=cache,uid=${UID},gid=${GID},target=/project/pdo/.cache/pip \
+    /project/pdo/run/bin/pip install notebook papermill
+
+# -----------------------------------------------------------------
+# Set up the contract source and configure for specified tests
+# -----------------------------------------------------------------
+
+# this code assumes that the Makefile creates a directory with the
+# contract source and mounts that directory
+
+# copy the source files into the image
+WORKDIR /project/pdo
+COPY --chown=${UNAME}:${UNAME} contracts /project/pdo/contracts
+
+# Local.cmake allows for custom overrides of the contracts to be
+# tested, the CONTRACT_FAMILIES variable in the cmake files specifies
+# which families should be built.
+#ARG CONTRACT_FAMILIES="exchange-contract digital-asset-contract inference-contract"
+#RUN echo "SET(CONTRACT_FAMILIES ${CONTRACT_FAMILIES})" > /project/pdo/contracts/Local.cmake
+
+# copy the tools because we want to be able to use them even without a
+# mount point after the container is created
+WORKDIR /project/pdo/tools
+COPY --chown=${UNAME}:${UNAME} tools/*.sh ./
+
+# build it!!!
+ARG UID=1000
+ARG GID=${UID}
+RUN --mount=type=cache,uid=${UID},gid=${GID},target=/project/pdo/.cache/pip \
+    /project/pdo/tools/build_contracts.sh
+
+ENTRYPOINT [ "/bin/bash" ]
---
+++ docker/tools/start_jupyter.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+# Copyright 2024 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SCRIPT_NAME=$(basename ${BASH_SOURCE[-1]} )
+
+source /project/pdo/tools/environment.sh
+source ${PDO_HOME}/bin/lib/common.sh
+
+# -----------------------------------------------------------------
+# Process command line arguments
+# -----------------------------------------------------------------
+F_LEDGER_URL=
+F_MODE=build
+F_SERVICE_HOST=
+
+F_USAGE='-1|--ledger [url] -m|--mode [build|copy|skip] -s|--service-host [hostname]'
+SHORT_OPTS='l:m:s:'
+LONG_OPTS='ledger:,mode:,service-host:'
+
+TEMP=$(getopt -o ${SHORT_OPTS} --long ${LONG_OPTS} -n "${SCRIPT_NAME}" -- "$@")
+if [ $? != 0 ] ; then echo "Usage: ${SCRIPT_NAME} ${F_USAGE}" >&2 ; exit 1 ; fi
+
+eval set -- "$TEMP"
+while true ; do
+    case "$1" in
+        -l|--ledger) F_LEDGER_URL="$2" ; shift 2 ;;
+        -m|--mode) F_MODE="$2" ; shift 2 ;;
+        -s|--service-host) F_SERVICE_HOST="$2" ; shift 2 ;;
+        --help) echo "Usage: ${SCRIPT_NAME} ${F_USAGE}"; exit 0 ;;
+    	--) shift ; break ;;
+    	*) echo "Internal error!" ; exit 1 ;;
+    esac
+done
+
+if [ -z "${F_SERVICE_HOST}" ]; then
+    die Missing required parameter: service-host
+fi
+
+if [ -z "${F_LEDGER_URL}" ]; then
+    die Missing required parameter: ledger
+fi
+
+export PDO_HOSTNAME=${F_SERVICE_HOST}
+export PDO_LEDGER_URL=${F_LEDGER_URL}
+
+export no_proxy=$PDO_HOSTNAME,$no_proxy
+export NO_PROXY=$PDO_HOSTNAME,$NO_PROXY
+
+check_pdo_runtime_env
+
+# -----------------------------------------------------------------
+yell copy ledger keys
+# -----------------------------------------------------------------
+if [ ! -f ${XFER_DIR}/ccf/keys/networkcert.pem ]; then
+    die "unable to locate ledger keys"
+fi
+mkdir -p ${PDO_LEDGER_KEY_ROOT}/
+try cp ${XFER_DIR}/ccf/keys/networkcert.pem ${PDO_LEDGER_KEY_ROOT}/
+
+# -----------------------------------------------------------------
+# Handle the configuration of the services
+# -----------------------------------------------------------------
+if [ "${F_MODE,,}" == "build" ]; then
+    yell configure client for host $PDO_HOSTNAME and ledger $PDO_LEDGER_URL
+    try ${PDO_INSTALL_ROOT}/bin/pdo-configure-users -t ${PDO_SOURCE_ROOT}/build/template -o ${PDO_HOME} \
+        --key-count 10 --key-base user --host ${PDO_HOSTNAME}
+elif [ "${F_MODE,,}" == "copy" ]; then
+    yell copy the configuration from xfer/client/etc and xfer/client/keys
+    mkdir -p ${PDO_HOME}/etc ${PDO_HOME}/keys
+    cp ${XFER_DIR}/client/etc/* ${PDO_HOME}/etc/
+    cp ${XFER_DIR}/client/keys/* ${PDO_HOME}/keys/
+elif [ "${F_MODE,,}" == "skip" ]; then
+    yell start with existing configuration
+else
+    die "invalid restart mode; ${F_MODE}"
+fi
+
+# -----------------------------------------------------------------
+# -----------------------------------------------------------------
+export NOTEBOOK_ROOT=/project/pdo/notebooks
+
+if [ ${F_MODE} == 'build' ]; then
+    # clean out any existing notebooks
+    rm -rf ${NOTEBOOK_ROOT}/*
+
+    # create the directory tree
+    mkdir -p ${NOTEBOOK_ROOT}
+    mkdir -p ${NOTEBOOK_ROOT}/factories
+    mkdir -p ${NOTEBOOK_ROOT}/instances
+    mkdir -p ${NOTEBOOK_ROOT}/templates
+
+    cp /project/pdo/contracts/exchange-contract/docs/notebooks/factories/*.ipynb ${NOTEBOOK_ROOT}/factories
+    cp /project/pdo/contracts/exchange-contract/docs/notebooks/templates/*.ipynb ${NOTEBOOK_ROOT}/templates
+fi
+
+# -----------------------------------------------------------------
+yell run the service test suite
+# -----------------------------------------------------------------
+. ${PDO_INSTALL_ROOT}/bin/activate
+
+cd /project/pdo/notebooks
+jupyter lab --no-browser --port=8888
