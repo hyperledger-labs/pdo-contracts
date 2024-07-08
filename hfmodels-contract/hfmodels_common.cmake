986f4d25165b8e687ae6e269b60704b8f5320564
Prakash Narayana Moorthy <prakash.narayana.moorthy@intel.com>
2024-07-08 03:13:07 +0000

    Token object contract related files for tokenizing Hugging Face models. The token object implements the policy for accessing HF hosted models. The token object code inherits several base methods from the exchange contract token object, and only implements asset-use specific methods. In addition, the token object initialization method is used to store asset (HF model) specifc details, so of which are secrets, and others used to provide meta data regarding the asset for a prospective token user
    Signed-off-by: Prakash Narayana Moorthy <prakash.narayana.moorthy@intel.com>

--- hfmodels-contract/hfmodels_common.cmake
+++ hfmodels-contract/hfmodels_common.cmake
@@ -1,4 +1,4 @@
-# Copyright 2023 Intel Corporation
+# Copyright 2024 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,21 +19,21 @@ ENDIF()
 # ---------------------------------------------
 # Set up the include list
 # ---------------------------------------------
-SET (INFERENCE_INCLUDES ${WASM_INCLUDES})
-LIST(APPEND INFERENCE_INCLUDES ${EXCHANGE_INCLUDES})
-LIST(APPEND INFERENCE_INCLUDES ${CMAKE_CURRENT_LIST_DIR})
+SET (HFMODELS_INCLUDES ${WASM_INCLUDES})
+LIST(APPEND HFMODELS_INCLUDES ${EXCHANGE_INCLUDES})
+LIST(APPEND HFMODELS_INCLUDES ${CMAKE_CURRENT_LIST_DIR})
 
 # ---------------------------------------------
 # Set up the default source list
 # ---------------------------------------------
-FILE(GLOB INFERENCE_COMMON_SOURCE ${CMAKE_CURRENT_LIST_DIR}/inference/common/*.cpp)
-FILE(GLOB INFERENCE_CONTRACT_SOURCE ${CMAKE_CURRENT_LIST_DIR}/inference/contracts/*.cpp)
+FILE(GLOB HFMODELS_COMMON_SOURCE ${CMAKE_CURRENT_LIST_DIR}/hfmodels/common/*.cpp)
+FILE(GLOB HFMODELS_CONTRACT_SOURCE ${CMAKE_CURRENT_LIST_DIR}/hfmodels/contracts/*.cpp)
 
-SET (INFERENCE_SOURCES)
-LIST(APPEND INFERENCE_SOURCES ${INFERENCE_COMMON_SOURCE})
-LIST(APPEND INFERENCE_SOURCES ${INFERENCE_CONTRACT_SOURCE})
+SET (HFMODELS_SOURCES)
+LIST(APPEND HFMODELS_SOURCES ${HFMODELS_COMMON_SOURCE})
+LIST(APPEND HFMODELS_SOURCES ${HFMODELS_CONTRACT_SOURCE})
 
 # ---------------------------------------------
 # Build the wawaka contract common library
 # ---------------------------------------------
-SET(INFERENCE_LIB ww_inference)
+SET(HFMODELS_LIB ww_hfmodels)
