09b5fc80ae459062eabcc63f9353070bda1ae0c5
Mic Bowman <mic.bowman@intel.com>
2024-10-02 10:49:25 -0600

    Create a "contract" for common python modules
    Split out the "contracts" module from the exchange contract and put it
    into a new "common" contract family. While strictly speaking, this is
    not a contract family, the build processes incorporat module creation
    very easily. In addition, this provides a framework for adding common
    functionality (like wrappers for tests, shared methods, etc).
    
    Signed-off-by: Mic Bowman <mic.bowman@intel.com>

--- common-contract/setup.py
+++ common-contract/setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2018 Intel Corporation
+# Copyright 2022 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -19,61 +19,68 @@ import sys
 import subprocess
 import warnings
 
+## -----------------------------------------------------------------
+# Change values of the following variables to customize your
+# contract family; the contract_scripts variable is a list of
+# bash commands that will be created in the python wheel. See
+# ./pdo/{contract_family}/scripts/scripts.py for more information
+# on building command line scripts.
+## -----------------------------------------------------------------
+contract_family = 'contracts'
+contract_scripts = [ ]
+
+author = 'Mic Bowman, Intel Labs'
+author_email = 'mic.bowman@intel.com'
+author_url = 'http://www.intel.com'
+
+## -----------------------------------------------------------------
+# Unless you change the standard behavior or layout of the contract
+# there should be no changes required below
+## -----------------------------------------------------------------
+
 # this should only be run with python3
-import sys
 if sys.version_info[0] < 3:
     print('ERROR: must run with python3')
     sys.exit(1)
 
-from setuptools import setup, find_packages, find_namespace_packages
+from setuptools import setup
 
-# -----------------------------------------------------------------
+## -----------------------------------------------------------------
 # Versions are tied to tags on the repository; to compute correctly
 # it is necessary to be within the repository itself hence the need
 # to set the cwd for the bin/get_version command.
-# -----------------------------------------------------------------
+## -----------------------------------------------------------------
 root_dir = os.path.dirname(os.path.realpath(__file__))
 try :
     pdo_contracts_version = subprocess.check_output(
         'bin/get_version', cwd=os.path.join(root_dir, os.pardir)).decode('ascii').strip()
 except Exception as e :
-    warnings.warn('Failed to get pdo_contracts version, using the default')
+    warnings.warn(f'Failed to get pdo_contracts version, using the default; {e}')
     pdo_contracts_version = '0.0.0'
 
 try :
     pdo_client_version = subprocess.check_output(
         'bin/get_version', cwd=os.path.join(root_dir, os.pardir, 'private-data-objects')).decode('ascii').strip()
 except Exception as e :
-    warnings.warn('Failed to get pdo_client version, using the default')
+    warnings.warn(f'Failed to get pdo_client version, using the default; {e}')
     pdo_client_version = '0.0.0'
 
 ## -----------------------------------------------------------------
 ## -----------------------------------------------------------------
 setup(
-    name='pdo_exchange',
+    name=f'pdo_{contract_family}',
     version=pdo_contracts_version,
-    description='Support functions PDO exchange contracts',
-    author='Mic Bowman, Intel Labs',
-    author_email='mic.bowman@intel.com',
-    url='http://www.intel.com',
+    description='Contract and support scripts for a PDO contract',
+    author=author,
+    author_email=author_email,
+    url=author_url,
     package_dir = {
         'pdo' : 'pdo',
-        'pdo.exchange.resources.etc' : 'etc',
-        'pdo.exchange.resources.context' : 'context',
-        'pdo.exchange.resources.contracts' : '../build/exchange-contract',
-        'pdo.exchange.resources.scripts' : 'scripts',
     },
     packages = [
         'pdo',
-        'pdo.exchange',
-        'pdo.exchange.jupyter',
-        'pdo.exchange.plugins',
-        'pdo.exchange.scripts',
-        'pdo.exchange.resources',
-        'pdo.exchange.resources.etc',
-        'pdo.exchange.resources.context',
-        'pdo.exchange.resources.contracts',
-        'pdo.exchange.resources.scripts',
+        f'pdo.{contract_family}',
+        f'pdo.{contract_family}.jupyter',
     ],
     include_package_data=True,
     install_requires = [
@@ -81,17 +88,7 @@ setup(
         'ipywidgets',
         'pdo-client>=' + pdo_client_version,
         'pdo-common-library>=' + pdo_client_version,
-        'pdo-sservice>=' + pdo_client_version,
     ],
     entry_points = {
-        'console_scripts' : [
-            'ex_asset_type=pdo.exchange.scripts.scripts:asset_type',
-            'ex_vetting=pdo.exchange.scripts.scripts:vetting',
-            'ex_issuer=pdo.exchange.scripts.scripts:issuer',
-            'ex_guardian=pdo.exchange.scripts.scripts:guardian',
-            'ex_token_issuer=pdo.exchange.scripts.scripts:token_issuer',
-            'ex_token_object=pdo.exchange.scripts.scripts:token_object',
-            'ex_exchange=pdo.exchange.scripts.scripts:exchange',
-        ]
     }
 )
